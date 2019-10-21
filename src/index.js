a0c3f2eb42e5115f5d740ea98d0b8226802cd857
Adebayo Adekoya <adekoya.adebayojubril@gmail.com>
2019-10-21 09:49:46 +0100

    version 1.0.0

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './bootstrap.css';
+import './style.css';
+// import './font-awesome.css';
+import './fontawesome-webfont3e6e.eot';
+import Counters from './components/counters'
+import App from './header'
+
+
+ReactDOM.render(<App />, document.getElementById('root'));	
